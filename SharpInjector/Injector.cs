ca26fd6512ff00d4295d30dd3d290136c43f0852
Josh <26735119+Akaion@users.noreply.github.com>
2018-12-18 21:43:27 +1300

    Improved codebase

--- SharpInjector/Injector.cs
+++ SharpInjector/Injector.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Text;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -7,120 +8,104 @@ namespace SharpInjector
 {
     public class Injector
     {
-        private const uint MEM_COMMIT = 0x00001000;
-        private const uint MEM_RESERVE = 0x00002000;
+        [Flags]
+        private enum MemProtection
+        {
+            PageExecuteReadWrite = 0x040
+        }
         
-        private const uint MEM_RELEASE = 0x8000;
-
-        private const uint PAGE_EXECUTE_READWRITE = 0x40;
-
-        private const uint PROCESS_VM_OPERATION = 0x0008;
-        private const uint PROCESS_CREATE_THREAD = 0x0002;
-        private const uint PROCESS_VM_WRITE = 0x0020;
-
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr VirtualAllocEx(IntPtr handle, IntPtr address, uint size, uint allocType, uint protect);
+        [Flags]
+        private enum MemoryAllocation
+        {
+            Commit = 0x01000,
+            Reserve = 0x02000,
+            Release = 0x08000
+        }
 
         [DllImport("kernel32.dll")]
-        private static extern bool WriteProcessMemory(IntPtr handle, IntPtr baseAddress, IntPtr buffer, uint size, out uint bytesWritten);
+        private static extern IntPtr VirtualAllocEx(IntPtr handle, IntPtr address, int size, MemoryAllocation allocType, MemProtection protect);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool WriteProcessMemory(IntPtr handle, IntPtr baseAddress, byte[] buffer, int size, int bytesWritten);
 
         [DllImport("kernel32.dll")]
         private static extern IntPtr CreateRemoteThread(IntPtr handle, IntPtr threadAttributes, uint stackSize, IntPtr startAddress, IntPtr parameter, uint creationFlags, IntPtr threadId);
 
         [DllImport("kernel32.dll")]
-        private static extern IntPtr LoadLibraryA(IntPtr name);
+        private static extern IntPtr GetProcAddress(IntPtr moduleHandle, string name);
 
         [DllImport("kernel32.dll")]
-        private static extern IntPtr GetProcAddress(IntPtr moduleHandle, IntPtr name);
-
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr OpenProcess(uint access, bool inheritHandle, uint pid);
+        private static extern IntPtr GetModuleHandle(string moduleName);
 
         [DllImport("kernel32.dll")]
         private static extern bool CloseHandle(IntPtr handle);
 
         [DllImport("kernel32.dll")]
-        private static extern bool VirtualFreeEx(IntPtr handle, IntPtr address, uint size, uint freeType);
+        private static extern bool VirtualFreeEx(IntPtr handle, IntPtr address, int size, MemoryAllocation freeType);
 
         public bool Inject(string dllPath, int pid)
         {
             bool result = false;
 
-            IntPtr processHandle = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE, false, (uint) pid);
-
+            // Get a handle to the process
+            
+            var processHandle = Process.GetProcessById(pid).Handle;
+            
             if (processHandle == IntPtr.Zero)
             {
                 MessageBox.Show("Failed to open valid process handle!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return result;
             }
 
-            byte[] asciiDllPath = Encoding.ASCII.GetBytes((dllPath + "\0"));
-            byte[] asciiKernel32DLL = Encoding.ASCII.GetBytes("kernel32.dll\0");
-            byte[] asciiLoadLibraryA = Encoding.ASCII.GetBytes("LoadLibraryA\0");
+            // Get the address of load library
             
-            IntPtr dllPathBuffer = Marshal.AllocHGlobal(asciiDllPath.Length);
-            IntPtr kernel32Buffer = Marshal.AllocHGlobal(asciiKernel32DLL.Length);
-            IntPtr loadLibraryABuffer = Marshal.AllocHGlobal(asciiLoadLibraryA.Length);
+            var loadLibraryAddr = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryW");
 
-            Marshal.Copy(asciiDllPath, 0, dllPathBuffer, asciiDllPath.Length);
-            Marshal.Copy(asciiKernel32DLL, 0, kernel32Buffer, asciiKernel32DLL.Length);
-            Marshal.Copy(asciiLoadLibraryA, 0, loadLibraryABuffer, asciiLoadLibraryA.Length);
-
-            IntPtr kernel32Base = LoadLibraryA(kernel32Buffer);
-
-            if (kernel32Base != IntPtr.Zero)
+            if (loadLibraryAddr == IntPtr.Zero)
             {
-                IntPtr loadLibraryAddr = GetProcAddress(kernel32Base, loadLibraryABuffer);
-
-                if (loadLibraryAddr != IntPtr.Zero)
-                {
-                    IntPtr remoteAddress = VirtualAllocEx(processHandle, IntPtr.Zero, (uint) asciiDllPath.Length, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-
-                    if (remoteAddress != IntPtr.Zero)
-                    {
-                        uint written;
-
-                        if (WriteProcessMemory(processHandle, remoteAddress, dllPathBuffer, (uint) asciiDllPath.Length, out written))
-                        {
-                            IntPtr remoteThread = CreateRemoteThread(processHandle, IntPtr.Zero, 0, loadLibraryAddr, remoteAddress, 0, IntPtr.Zero);
-                            
-                            if (remoteThread != IntPtr.Zero)
-                            {
-                                CloseHandle(remoteThread);
-
-                                VirtualFreeEx(processHandle, remoteAddress, (uint) asciiDllPath.Length, MEM_RELEASE);
-
-                                result = true;
-                            }
-                            else
-                            {
-                                MessageBox.Show("Failed to create remote thread!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                            }
-                        }
-                        else
-                        {
-                            MessageBox.Show("Failed to write data to remote process!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        }
-                    } else
-                    {
-                        MessageBox.Show("Failed to allocate memory in remote process!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Failed to get kernel32.dll!LoadLibraryA address!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
+                MessageBox.Show("Failed to get load library address!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return result;
             }
-            else
+            
+            // Allocate memory for the dll path in the process
+            
+            var remoteAddress = VirtualAllocEx(processHandle, IntPtr.Zero, dllPath.Length, MemoryAllocation.Commit | MemoryAllocation.Reserve, MemProtection.PageExecuteReadWrite);
+
+            if (remoteAddress == IntPtr.Zero)
             {
-                MessageBox.Show("Failed to get kernel32.dll base address!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Failed to allocate memory for the dll!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return result;
+            }
+            
+            // Write the dll path into the process
+
+            var dllPathBuffer = Encoding.Unicode.GetBytes(dllPath + "\0");
+            
+            if(!WriteProcessMemory(processHandle, remoteAddress, dllPathBuffer, dllPathBuffer.Length, 0))
+            {
+                MessageBox.Show("Failed to write memory!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return result;
+            }
+            
+            // Create a remote thread to call the dll
+            
+            var remoteThread = CreateRemoteThread(processHandle, IntPtr.Zero, 0, loadLibraryAddr, remoteAddress, 0, IntPtr.Zero);
+
+            if (remoteThread == IntPtr.Zero)
+            {
+                MessageBox.Show("Failed to create remote thread!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return result;
             }
 
-            CloseHandle(processHandle);
+            // Close the handle
+            
+            CloseHandle(remoteThread);
 
-            Marshal.FreeHGlobal(dllPathBuffer);
-            Marshal.FreeHGlobal(kernel32Buffer);
-            Marshal.FreeHGlobal(loadLibraryABuffer);
+            // Free the memory
+            
+            VirtualFreeEx(processHandle, remoteAddress, dllPath.Length, MemoryAllocation.Release);
+            
+            result = true;
 
             return result;
         }
